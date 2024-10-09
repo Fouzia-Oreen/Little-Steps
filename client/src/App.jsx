3154eac63d1ca26e1ff49565148f038a8565127f
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-09 20:18:24 +0600

    shadcn added and file is configured

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import Footer from './components/Footer'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
+    <>
    <h1>Hello Little steps</h1>
+   <Button className="bg-red-400">Check</Button>
+   <Footer />
+   
+   </>
   )
 }
 
 export default App
+//
