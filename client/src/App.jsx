a7a9228a105f7495f924f450d3d537d9b008edd3
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-16 18:49:09 +0600

    pages render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,10 @@ import Footer from './components/footer'
 import AuthPage from './pages/AuthPage'
 import HomePage from './pages/HomePage'
 import Header from './components/header/Header'
-import RouteGaurd from './components/route-gaurd'
+import RouteGaurd from './components/route-gaurd/index.js'
 import { useContext } from 'react'
 import { AuthContext } from './context/authContext'
-import InstructorDashboard from './pages/instructor/Dashboard'
+import InstructorDashboardPage from './pages/instructor/InstructorDashboardPage'
 import StudentViewCommonLayout from './components/student-view/StudentViewCommonLayout'
 import StudentHomePage from './pages/student/StudentHomePage'
 
@@ -18,27 +18,34 @@ function App() {
     <Header />
     <Routes >
       <Route path="/" element={<HomePage />}/>
-      <Route path="/auth" 
-        element={<RouteGaurd 
-        element={<AuthPage />} 
-        authenticated={auth.authenticated} 
+      <Route 
+        path="/auth" 
+        element={
+          <RouteGaurd 
+          element={<AuthPage/>} 
+          authenticated={auth?.authenticated} 
         user={auth?.user} />}
       />
-      <Route path="/instructor" 
-        element={<RouteGaurd 
-        element={<InstructorDashboard />} 
-        authenticated={auth.authenticated} 
+      <Route 
+        path="/instructor" 
+        element={
+          <RouteGaurd 
+          element={<InstructorDashboardPage/>} 
+          authenticated={auth?.authenticated} 
         user={auth?.user} />}
       />
-      <Route path="/" 
-        element={<RouteGaurd 
-        element={<StudentViewCommonLayout />} 
-        authenticated={auth.authenticated} 
+      <Route 
+        path="/" 
+        element={
+          <RouteGaurd 
+          element={<StudentViewCommonLayout />} 
+          authenticated={auth?.authenticated} 
         user={auth?.user} />}
       >
         <Route path="home" element={<StudentHomePage />}/>
         <Route path="" element={<StudentHomePage />}/>
       </Route>
+
     </Routes>
     <Footer />
    </>
--- client/src/components/route-gaurd/index.js
+++ client/src/components/route-gaurd/index.js
@@ -3,17 +3,19 @@ import { Fragment } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
 
-function RouteGaurd({authenticated, user, element}) {
+export const RouteGaurd = ({authenticated, user, element}) => {
     const location = useLocation()
     if (!authenticated && !location.pathname.includes('/auth')) {
         return <Navigate to='/auth' />
     }
-    if (authenticated && user?.role != 'instructor' && (location.pathname.includes('instructor') || location.pathname.includes('/auth'))) {
+    if (authenticated && 
+        user?.role != 'instructor' &&
+        (location.pathname.includes('instructor') || 
+        location.pathname.includes('/auth'))) {
         return <Navigate to='/home' />
     }
     if (authenticated && user?.role != 'instructor' &&  location.pathname.includes('instructor')) {
         return <Navigate to='/instructor' />
     }
-    return <Fragment>{ element } </Fragment>
+    return (<Fragment>{element}</Fragment>)
 }
-export default RouteGaurd
