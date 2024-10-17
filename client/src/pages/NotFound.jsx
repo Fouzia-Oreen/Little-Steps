688ce88bdd00c0326deaadff65c5c108d2ff299d
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-17 22:15:05 +0600

    Not-found page created

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+
+const NotFoundPage = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="font-bold text-4xl">Page Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
