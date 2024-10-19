ce3837c5def84461b4d4e2e86fd816bd0e0d1b93
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-19 20:28:40 +0600

    Students-home-page code added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,22 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import AuthProvider from './context/authContext.jsx'
 import InstructorProvider from './context/instructorContext.jsx'
+import StudentProvider from './context/studentContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-//   <BrowserRouter>
-//   <AuthProvider>
-//     <InstructorProvider>
-//       <StudentProvider>
-//         <App />
-//       </StudentProvider>
-//     </InstructorProvider>
-//   </AuthProvider>
-// </BrowserRouter>
   <BrowserRouter>
-    <AuthProvider>
+  <AuthProvider>
     <InstructorProvider>
-      <App />
-      </InstructorProvider>
-    </AuthProvider>
-  </BrowserRouter>,
+      <StudentProvider>
+        <App />
+      </StudentProvider>
+    </InstructorProvider>
+  </AuthProvider>
+</BrowserRouter>
+ 
 )
