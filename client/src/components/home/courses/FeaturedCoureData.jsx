c77e6bb9b1024b842c200b9f06f536aa9db79dae
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-22 00:01:44 +0600

    featured section on home page

---
+++ client/src/components/home/courses/FeaturedCoureData.jsx
@@ -0,0 +1,11 @@
+
+
+export const FeaturedCoureData = [
+    {
+        image:"",
+        catagory:"JavaScript",
+        total:"7 courses",
+        title:"Full Stack JavaScript",
+        desc:"This course takes you through our entire JavaScript curriculum. The courses should be taken in the order that they are displayed. You'll learn everything you need to know to create beautiful responsive websites from scratch using JavaScript and NodeJS."
+    }
+]
---
+++ client/src/pages/home/FeaturedCoursesSection.jsx
@@ -0,0 +1,22 @@
+
+const FeaturedCourses = () => {
+  return (
+    <section className="min-h-screen bg-blue-500">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="p-4 bg-yellow-200">
+        <img src="" alt="" />
+        <span>User Experience</span>
+        <h1>Fundamental of UX for Application design</h1>
+        <p>The automated process all your website tasks. Discover tools and techniques to engage effectively with vulnerable children and young people.</p>
+        <div>
+          <span>(4.5) based on 120</span>
+          <span>$135</span>
+        </div>
+        <button>Add to Cart</button>
+      </div>
+      </div>
+    </section>
+  )
+}
+
+export default FeaturedCourses
