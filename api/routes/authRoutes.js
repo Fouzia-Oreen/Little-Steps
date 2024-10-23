d96063ad6905eb3872c3377f515ea4d70b4b2685
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-23 22:13:53 +0600

    updated

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-const router = express.Router();
 import { loginUser, registerUser } from "../controllers/authController.js";
 import authinticateMiddleware from '../middleware/authMiddleware.js'
 
+// creating routes for register, login, check-authentication
+const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/check-auth", authinticateMiddleware, (req, res) => {
