92a7ec69e4d944c7430cb9d68641de74ac830b2e
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-23 22:00:20 +0600

    updated

--- api/helpers/cloudinary.js
+++ api/helpers/cloudinary.js
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import {v2 as cloudinary} from "cloudinary"
+
+// configuring cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
 })
+
+// upload media files to cloudinary
 export const uploadMediaToCloudinary = async (filePath) => {
     try {
         const result = await cloudinary.uploader.upload(filePath, {
@@ -18,9 +22,10 @@ export const uploadMediaToCloudinary = async (filePath) => {
     }
 }
 
+// delete media files from cloudinary
 export const deleteMediaFromCloudinary = async (publicId) => {
     try {
-       await cloudinary.uploader.destroy(filePath)
+       await cloudinary.uploader.destroy(publicId)
         return result
     } catch (error) {
         console.log(error)
