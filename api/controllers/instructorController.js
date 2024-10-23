cd847907dc2749e358145a5488fef08c6fe1ee9e
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-23 21:47:30 +0600

    updated

--- api/controllers/instructorController.js
+++ api/controllers/instructorController.js
@@ -1,27 +1,29 @@
 import Course from "../models/Course.js";
 
+// instructor adds a new course
 export const addNewCourse = async (req, res) => {
-  try {
-    const courseData = req.body;
-    const newlyCreatedCourse = new Course(courseData);
-    const saveCourse = await newlyCreatedCourse.save();
+    try {
+      const courseData = req.body;
+      const newlyCreatedCourse = new Course(courseData);
+      const saveCourse = await newlyCreatedCourse.save();
 
-    if (saveCourse) {
-      res.status(201).json({
-        success: true,
-        message: "Course saved successfully",
-        data: saveCourse,
+      if (saveCourse) {
+        res.status(201).json({
+          success: true,
+          message: "Course saved successfully",
+          data: saveCourse,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
+        success: false,
+        message: "Some error occured!",
       });
     }
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      success: false,
-      message: "Some error occured!",
-    });
-  }
 };
 
+// get all courses
 export const getAllCourses = async (req, res) => {
   try {
     const coursesList = await Course.find({});
@@ -39,66 +41,61 @@ export const getAllCourses = async (req, res) => {
   }
 };
 
+// course details updated
 export const getCourseDetailsByID = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const courseDetails = await Course.findById(id);
+    try {
+      const { id } = req.params;
+      const courseDetails = await Course.findById(id);
 
-    if (!courseDetails) {
-      return res.status(404).json({
+      if (!courseDetails) {
+        return res.status(404).json({
+          success: false,
+          message: "Course not found!",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        data: courseDetails,
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
         success: false,
-        message: "Course not found!",
+        message: "Some error occured!",
       });
     }
-
-    res.status(200).json({
-      success: true,
-      data: courseDetails,
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      success: false,
-      message: "Some error occured!",
-    });
-  }
 };
 
+// course update
 export const updateCourseByID = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedCourseData = req.body;
+    try {
+      const { id } = req.params;
+      const updatedCourseData = req.body;
 
-    const updatedCourse = await Course.findByIdAndUpdate(
-      id,
-      updatedCourseData,
-      { new: true }
-    );
+      const updatedCourse = await Course.findByIdAndUpdate(
+        id,
+        updatedCourseData,
+        { new: true }
+      );
 
-    if (!updatedCourse) {
-      return res.status(404).json({
+      if (!updatedCourse) {
+        return res.status(404).json({
+          success: false,
+          message: "Course not found!",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Course updated successfully",
+        data: updatedCourse,
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
         success: false,
-        message: "Course not found!",
+        message: "Some error occured!",
       });
     }
-
-    res.status(200).json({
-      success: true,
-      message: "Course updated successfully",
-      data: updatedCourse,
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      success: false,
-      message: "Some error occured!",
-    });
-  }
 };
-
-// export default {
-//   addNewCourse,
-//   getAllCourses,
-//   updateCourseByID,
-//   getCourseDetailsByID,
-// };
