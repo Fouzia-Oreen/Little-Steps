a38d843a6d5691894583ff6c553775f04cf1f564
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-23 22:22:52 +0600

    updated

--- api/routes/student-routes/courseRoute.js
+++ api/routes/student-routes/courseRoute.js
@@ -4,8 +4,10 @@ import {
   getAllStudentViewCourses,
   checkCoursePurchaseInfo,
 } from "../../controllers/student-controllers/courseController.js";
-const router = express.Router();
 
+
+const router = express.Router();
+// all the students can get/view courses its details, purchase info
 router.get("/get", getAllStudentViewCourses);
 router.get("/get/details/:id", getStudentViewCourseDetails);
 router.get("/purchase-info/:id/:studentId", checkCoursePurchaseInfo);
