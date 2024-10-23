89a163a5fcdbe7d2460c2885ffabcf93e0d8ba72
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-10-23 22:26:24 +0600

    updated

--- api/routes/student-routes/studentCourseRoute.js
+++ api/routes/student-routes/studentCourseRoute.js
@@ -1,10 +1,9 @@
 import express from "express";
 import {
   getCoursesByStudentId,
-} from "../../controllers/student-controllers/studentCourseController";
+} from "../../controllers/student-controllers/studentCourseController.js";
 
 const router = express.Router();
-
 router.get("/get/:studentId", getCoursesByStudentId);
 
-module.exports = router;
+export default router;
